6f472450ba594ac2966d71df325537b4a8417296
Colin Ciervo <colinciervo@@gmail.com>
2022-06-10 15:22:38 -0400

    Mostly done

--- oop.js
+++ oop.js
@@ -2,16 +2,41 @@
 // it should have a dynamic name property
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class Unicorn {
+  constructor(horn) {
+    this.horn = horn;
+    this.color = 'Silver';
+    this.message =  'message';
+  }
+  say(say) {
+    this.message = say;
+  }
+}
+var unicorn1 = new Unicorn('white');
+console.log(unicorn1);
 
-
+unicorn1.say('Unicorn type beat');
+console.log(unicorn1);
 
 // Write a class called Vampire
 // it should have a dynamic name property
 // it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
-
-
+class Vampire {
+  constructor(name, pet, thirsty) {
+    this.name = name;
+    this.pet = 'bat';
+    this.thirsty = true;
+  }
+  drink(thirst) {
+    this.thirsty = false
+  }
+}
+var vamp1 = new Vampire ('Paul');
+console.log(vamp1);
+vamp1.drink();
+console.log(vamp1);
 
 //  Write a Dragon class
 // it should have a dynamic name property (string)
@@ -21,6 +46,38 @@
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
 
+  class Dragon {
+    constructor(name, rider, color) {
+      this.name = name;
+      this.rider = rider;
+      this.color = color;
+      this.ishungry = true;
+      this.tummy = 0;
+    }
+    food(yum) {
+      this.ishungry = false
+    }
+    eat(tummy) {
+      this.tummy = this.tummy +1
+    }
+  }
+  var drag1 = new Dragon ('Greg', 'Rick', 'green');
+  console.log(drag1);
+  drag1.eat();
+  console.log(drag1);
+  drag1.eat();
+  console.log(drag1);
+  drag1.eat();
+  console.log(drag1);
+  drag1.eat();
+  if (this.tummy = 4) {
+    drag1.food();
+  }
+  console.log(drag1);
+  if (this.tummy = 4) {
+    console.log('No longer hungry')
+  }
+  console.log(drag1);
 
 
 //  BIG CHALLNEGE: Write a Hobbit class
@@ -31,3 +88,40 @@
 //  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 //  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
 //  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit {
+  constructor (name, dispo) {
+    this.name = name;
+    this.disposition = dispo;
+    this.age = 33;
+    this.isAdult = false;
+    this.isOld = false;
+    this.hasRing = false;
+  }
+  celebrateBirthday(age) {
+    this.age = this.age +1
+  }
+  adultMan(adult) {
+    this.isAdult = true
+  }
+  oldMan(old) {
+    this.isOld = true
+  }
+  oldTime(oneOhOne) {
+    this.age = this.age +68
+  }
+  oneRing(frodo) {
+    this.hasRing = true
+  }
+}
+
+
+var hob1 = new Hobbit ('Sam', 'Friendly');
+if (this.name = 'Frodo' ) {
+  hob1.oneRing();
+} else if (this.name != 'Frodo') {
+  console.log('Not the chosen one');
+}
+if (this.age = 33) {
+  hob1.adultMan();
+}
+console.log(hob1);
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+1. It was very challenging and extremely rewarding to diagnose the issues in my code. I got discourage a few times (like right now I'm taking a break from the final challenge on Oop.js).
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+2. I still need to work on taking breaks rather than just slamming my head into a wall when I get stuck on something. I have a bad habit of not giving up
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+3. I need someone to remind me to get away from my computer for a while. I need to remember to get enough sleep, stick to my bedtime and eat right. I think getting more used to working on a calendar will help me out a lot. I need to make sure I don't forget to schedule meals and breaks.
